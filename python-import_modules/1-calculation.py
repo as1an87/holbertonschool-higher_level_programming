b1950cafd1e4ff4992c235610684628c85529180
as1an87 <as1anoff87@gmail.com>
2024-05-19 12:59:32 +0000

    wrong calc

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         return (a * b)
     def div(a , b):
         return (a / b)
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} - {} = {}".format(a, b, add(a, b)))
+    print("{} * {} = {}".format(a, b, div(a, b)))
+    print("{} / {} = {}".format(a, b, mul(a, b)))
