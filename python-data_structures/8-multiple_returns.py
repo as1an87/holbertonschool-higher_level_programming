137dade846f972765234d5dc9ce5f5900f21abdc
as1an87 <as1anoff76@gmail.com>
2024-05-24 18:37:52 +0000

    first character

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first  = sentence[0]
+    if sentence != "":
+        length = len(sentence)
+        first  = sentence[0]
 
-    return length, first
+        return length, first
+    else:
+        length = 0
+        return length, None
