62fe87173582616a2e72b82679fc3d87260d383c
as1an87 <as1anoff87@gmail.com>
2024-05-25 21:22:03 +0000

    bestscore

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     best_score = 0
-    for value in a_dictionary.values():
+    best_key = None
+    for key, value in a_dictionary.items():
         if value > best_score:
             best_score = value
-    return best_score
+            best_key = key
+    return best_key
