239381942bb13e458af044b4f829027d0cdf33da
as1an87 <as1anoff87@gmail.com>
2024-05-25 18:45:02 +0000

    replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
 
-    new = [89 if x == 2 else x for x in my_list]
+    new = [replace if x == search else x for x in my_list]
     return new
