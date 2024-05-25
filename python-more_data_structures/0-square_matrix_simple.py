0568a981127d1d68ef80bdf335b29dcf10ee7640
as1an87 <as1anoff87@gmail.com>
2024-05-25 13:41:51 +0000

    square

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,11 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new = []
     for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(element)
-        new.append(new_row)
-    for x in range(len(new)):
-        for y in range(len(new)):
-            new[x][y] = new[x][y] ** 2
+        row = list(map(lambda x: x**2, row))
+        new.append(row)
     return new
