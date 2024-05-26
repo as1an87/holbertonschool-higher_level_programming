603edc77af19a1f00b9be615895d27bdee4e924a
as1an87 <as1anoff87@gmail.com>
2024-05-26 15:25:33 +0000

    safeprint

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         while i < x:
-            print("{:d}".format(my_list[i]), end = "")
+            print("{:d}".format(my_list[i]), end="")
             i += 1
     except IndexError:
         pass
-    
+
     print()
     return i
