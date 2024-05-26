ffe3568dfda4c4a8d098f3f31c0e06425ed62fc0
as1an87 <as1anoff87@gmail.com>
2024-05-26 15:09:43 +0000

    safeprint

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+   i = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
@@ -9,5 +10,7 @@ def safe_print_list(my_list=[], x=0):
         print()
     if x >= i:
         return i + 1
+    elif i == 0:
+        return i
     else:
         return x + 1
