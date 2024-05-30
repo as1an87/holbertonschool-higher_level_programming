b9b0838bd174d4c73b96ce53ae50fe77fe57c4b5
as1an87 <as1anoff87@gmail.com>
2024-05-30 20:35:35 +0000

    saymyname

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Print the full name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints the full name"""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name:
+        print("My name is {} {}.".format(first_name, last_name))
+    else:
+        print("My name is {}.".format(first_name))
