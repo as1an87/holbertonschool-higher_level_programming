994593ac45edac60a68f654c97c6528040f59d11
as1an87 <as1anoff87@gmail.com>
2024-05-30 13:34:57 +0000

    0addint

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    def cast_to_int(a, b):
+        if isinstance(a, float):
+            a = int(a)
+        if isinstance(b, float):
+            b = int(b)
+        return a, b
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    return a + b
