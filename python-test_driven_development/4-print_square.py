129304dcef83905e1658647815280e6cc64d0dbc
as1an87 <as1anoff87@gmail.com>
2024-05-30 21:15:23 +0000

    printqqare

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """
-print square
+    print square
 """
 
 
 def print_square(size):
+    """print square"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be integer")
     for i in range(size):
         [print("#", end="") for j in range(size)]
         print("")
