39bf5104d7e589a7c03d526fb26c2a66fa5067cc
as1an87 <as1anoff87@gmail.com>
2024-06-01 13:01:50 +0000

    6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,10 +6,11 @@
 number_of_instances = 0
 class Rectangle:
     """5"""
+    Rectangle.number_of_instances += 1
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
