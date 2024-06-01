5d1f918f0c749d84c0f30297de6f8b6c64c94011
as1an87 <as1anoff87@gmail.com>
2024-06-01 11:32:11 +0000

    2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -34,4 +34,7 @@ class Rectangle:
     def area(self):
         return self.width * self.height
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
