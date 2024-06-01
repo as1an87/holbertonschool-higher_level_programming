2de9f3d25a73dae143d88ef257e9df9f7b22d63e
as1an87 <as1anoff87@gmail.com>
2024-06-01 13:24:45 +0000

    7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -9,7 +9,7 @@ number_of_instances = 0
 class Rectangle:
     """5"""
     number_of_instances += 0
-    print_symbol = "#"
+    print_symbol = "C"
 
     def __init__(self, width=0, height=0):
         self.width = width
