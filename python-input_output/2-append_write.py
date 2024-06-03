c73468b3c0d06d50725b39b13568676c2917f849
as1an87 <as1anoff87@gmail.com>
2024-06-03 12:40:52 +0000

    append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+readfile
+"""
+
+
+def append_write(filename="", text=""):
+    """readfile"""
+    char_count = len(text)
+    with open(filename, mode='a', encoding='utf-8') as file:
+        file.write(text)
+        return chat_count
