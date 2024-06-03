3ca45b9931d347bc2ed96b07bce83db04d0fedc3
as1an87 <as1anoff87@gmail.com>
2024-06-03 11:21:10 +0000

    myfile

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+readfile
+"""
+
+
 def read_file(filename=""):
+    """readfile"""
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end='')
