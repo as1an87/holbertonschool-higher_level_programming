c1cfa0b3af6df64c077e7b608bab4446c995d396
as1an87 <as1anoff87@gmail.com>
2024-06-08 22:35:23 +0000

    json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+classtojson
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer, boolean) for JSON serialization of an object.
+
+    Parameters:
+    obj (object): An instance of a class.
+
+    Returns:
+    dict: A dictionary containing the serializable attributes of the object.
+    """
+    return obj.__dict__
+
