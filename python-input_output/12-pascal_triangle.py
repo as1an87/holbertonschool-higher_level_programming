3a95bdcfe31c9cae1890ef7f95a9dfe8a34cea03
as1an87 <as1anoff87@gmail.com>
2024-06-08 22:57:03 +0000

    pascal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,23 +7,22 @@
 def pascal_triangle(n):
     """
     Generate Pascal's triangle of n rows.
-    
+
     Parameters:
     - n (int): The number of rows in the triangle.
-    
+
     Returns:
     list: A list of lists representing Pascal's triangle.
     """
     triangle = []
     if n <= 0:
         return triangle
-    
+
     for i in range(n):
         row = [1] * (i + 1)
         if i >= 2:
             for j in range(1, i):
                 row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
-    
-    return triangle
 
+    return triangle
