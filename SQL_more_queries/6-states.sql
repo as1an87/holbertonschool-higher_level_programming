fefd714f555e0e24d303926b7a11f425fa99b00c
as1an87 <as1anoff87@gmail.com>
2024-06-29 14:41:24 +0000

    states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(256) NOT NULL,
+	PRIMARY KEY(`id`)
 );
