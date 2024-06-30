345bf64d3167593afbaad93f5b3f528b143430be
as1an87 <as1anoff87@gmail.com>
2024-06-30 18:37:37 +0000

    modelstate

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,25 +1,31 @@
-
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as argument
-from database `hbtn_0e_6_usa`
+Script that prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
 """
-from sys import argv
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    srch_state = sys.argv[4]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(usr, pwd, db_name))
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == argv[4]).first()
-    try:
-        print(instance.id)
-    except Exception:
-        print("Not found")
+    state = session.query(State).filter(State.name == srch_state).first()
 
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+    session.close()
